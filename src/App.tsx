a98c90b348d6306736697131ea71ac34bbce46ed
khanhtrantrong <100473227+khanhtrantrong@users.noreply.github.com>
2023-09-24 13:24:36 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+    console.log("phong ngu")
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
