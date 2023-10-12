44c8733ff05a0a79e11c441535e709b2bb52effb
DuyLT198 <letheduy1982003@gmail.com>
2023-10-12 09:23:52 +0700

    Create Chart.tsx
    update chart'

---
+++ src/pages/Chart/LineChart/Chart.tsx
@@ -0,0 +1,120 @@
+import { ApexOptions } from "apexcharts";
+import React, { useState } from "react";
+import ReactApexChart from "react-apexcharts";
+
+type chartype = {
+  series: any[]; // Chú ý kiểu dữ liệu của series phải phù hợp với ứng dụng của bạn
+  options: ApexOptions; // Sử dụng kiểu dữ liệu ApexOptions ở đây
+  selection: string;
+};
+
+const Chart: React.FC = () => {
+  const [series] = useState([
+    {
+      name: "Inflation",
+      data: [2.3, 3.1, 4.0, 1000, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2],
+    },
+  ]);
+
+  const [options] = useState<ApexOptions>({
+    chart: {
+      height: 250,
+      type: "bar",
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 10,
+        dataLabels: {
+          position: "top", // top, center, bottom
+        },
+      },
+    },
+    dataLabels: {
+      enabled: true,
+      formatter: function (val: any) {
+        return val + "vnđ";
+      },
+      offsetY: -20,
+      style: {
+        fontSize: "12px",
+        colors: ["#304758"],
+      },
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      position: "top",
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      crosshairs: {
+        fill: {
+          type: "gradient",
+          gradient: {
+            colorFrom: "#D8E3F0",
+            colorTo: "#BED1E6",
+            stops: [0, 100],
+            opacityFrom: 0.4,
+            opacityTo: 0.5,
+          },
+        },
+      },
+      tooltip: {
+        enabled: true,
+      },
+    },
+    yaxis: {
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      labels: {
+        show: false,
+        formatter: function (val: any) {
+          return val + "vnđ";
+        },
+      },
+    },
+    title: {
+      text: "Monthly Inflation in Argentina, 2002",
+      floating: true,
+      offsetY: 330,
+      align: "center",
+      style: {
+        color: "#444",
+      },
+    },
+  });
+
+  return (
+    <div className="w-full   " id="chart">
+      <div id="chart-timeline">
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="bar"
+          height={250}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Chart2;
